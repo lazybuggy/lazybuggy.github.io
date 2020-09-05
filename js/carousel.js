7589b44d04fbacf735925e116020367851b249b4
Lucia <lucia.okeh@gmail.com>
2020-09-05 00:17:24 -0400

    almost finalizing changes

--- js/carousel.js
+++ js/carousel.js
@@ -168,4 +168,17 @@ window.onload = function () {
       textSequence(0);
     }
   }
+
+  ///
+
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
+  //
 };
